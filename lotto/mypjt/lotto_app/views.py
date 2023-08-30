d932024cdd5edb160d6bfaa7e9b37e41bc034402
Crush_on_Study <99119068+Crush-on-Study@users.noreply.github.com>
2023-08-30 23:28:07 +0900

    Update views.py

--- lotto/mypjt/lotto_app/views.py
+++ lotto/mypjt/lotto_app/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from lotto_anal import analyze_lotto_numbers, get_lotto_numbers
 
-def lotto_analysis(request):
+async def lotto_analysis(request):
     start = 1025  # 시작 회차
     end = 1028  # 종료 회차
 
-    lotto_numbers = get_lotto_numbers(start, end)
+    lotto_numbers = await get_lotto_numbers(start, end)  # await 키워드 추가
+
     most_common_numbers = analyze_lotto_numbers(lotto_numbers)
 
     context = {
         'most_common_numbers': most_common_numbers
     }
 
-    return render(request, 'lotto_app/lotto_analysis.html', context)
+    return render(request, 'lotto_app/lotto_show.html', context)
